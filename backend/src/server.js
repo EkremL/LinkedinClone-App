e6e0379ed1252e0009369d4863ecc5441153faf8
EkremL <ekremlale1907@gmail.com>
2024-11-10 01:33:02 +0300

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.use(
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.resolve(__dirname, "../../frontend/dist")));
 
 //!Routes
 app.use("/api/auth", authRoutes);
