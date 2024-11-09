68e33ef43bdc50b008534cd58d77d9e0c605c522
EkremL <ekremlale1907@gmail.com>
2024-11-10 01:24:09 +0300

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
